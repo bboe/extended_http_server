34ad259cc9e05c63b0c4100c00b41b6f57a1f579
Bryce Boe <bbzbryce@gmail.com>
2012-10-30 12:45:45 -0700

    Add Simplified BSD License.

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
+import os
 import re
 from setuptools import setup
 
-version = re.search("__version__ = '([^']+)'",
-                    open('ext_http_server.py').read()).group(1)
+MODULE_NAME = 'ext_http_server'
 
-setup(name='ext_http_server',
-      version=version,
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+VERSION = re.search("__version__ = '([^']+)'",
+                    open('{0}.py'.format(MODULE_NAME)).read()).group(1)
+
+setup(name=MODULE_NAME,
       author='Bryce Boe',
       author_email='bbzbryce@gmail.com',
-      url = 'https://github.com/bboe/extended_http_server',
-      description = ('An extended version of python\'s SimpleHTTPServer that '
+      classifiers=['Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   #'Programming Language :: Python :: 3'
+                   ],
+      description=('An extended version of python\'s SimpleHTTPServer that '
                      'supports https, authentication, rate limiting, and '
                      'download resuming.'),
+      entry_points={'console_scripts': ['{0} = {0}:main'.format(MODULE_NAME)]},
       install_requires=[],
       keywords=['http resume', 'http rate limit', 'http authentication'],
-      py_modules=['ext_http_server'],
-      entry_points = {'console_scripts':
-                          ['ext_http_server = ext_http_server:main']}
-      )
+      license='Simplified BSD License',
+      long_description=README,
+      py_modules=[MODULE_NAME],
+      url = 'https://github.com/bboe/extended_http_server',
+      version=VERSION)
